141b4a427b1d92e35e48297269feea25a693f0f4
ReneDescarnage <Kyle.dean.sinclair@gmail.com>
2020-12-29 10:56:59 +0100

    Update snippets.java

--- snippets.java
+++ snippets.java
@@ -1,3 +1,11 @@
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+//Take an array of strings and manipulate individual characters using substrings
+return Arrays.stream(phrase.split(" "))
+                   .map(i -> i.substring(0, 1).toUpperCase() + i.substring(1, i.length()))
+                   .collect(Collectors.joining(" "));
+
 
 //Process an array of pairs of strings into separate lists 
 
